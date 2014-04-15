c6d2f0ab9f32cead375ef4d310712cc9b39c42f4
Trevor <trevorbroad84@gmail.com>
2014-04-15 01:32:09 -0700

    updates

--- infinite-carousel.js
+++ infinite-carousel.js
@@ -14,40 +14,37 @@ jQuery.fn.outerHTML = function(s) {
         : jQuery("<p>").append(this.eq(0).clone()).html();
 };
 
-
 (function($) {
   
   $.fn.infiniteCarousel = function(options) {
-    
-    var elements = {
-      carouselWrapper:  $(this),
-      thumbWrapper:     $('#infinite-thumbs'),
-      thumbContainer:   $('#infinite-thumbs li'),
-      excerptContent:   $('#infinite-content li'),
-      windowWidth:    	$(window).width()     
-    },    
-    defaults = {
+       
+    var defaults = {
       thumbsVisible:    5,
-      thumbContainer:   elements.thumbWrapper,
-      thumbWidth:     	elements.thumbContainer.outerWidth(true),
-      thumbCount:     	elements.thumbContainer.length,
       playSlider:     	true,
       slideTime:      	5000,
       transitionTime:   1300,
-      transitionType:   'easeOutQuad'
+      transitionType:   'easeOutQuad',
+      hoverFocus:		true,
+      centerClicked:	true,
+	  carouselWrapper:  $(this),
+      thumbWrapper:		$('#infinite-thumbs'),
+      thumbContainer:   $('#infinite-thumbs li'),
+      excerptContent:   $('#infinite-content li')
     },            
     options = $.extend(defaults, options),      
     settings = {
-      thumbFocus:   Math.round( options.thumbsVisible / 2 ),
-      slideAction:  null,
-      inTransition: null
+      thumbWidth:     	options.thumbContainer.outerWidth(true),
+      thumbCount:     	options.thumbContainer.length,
+      thumbFocus:   	Math.round( options.thumbsVisible / 2 ),
+      slideAction:  	null,
+      inTransition: 	null
     },              
     functions = {         
       init: function(){
         $('#infinite-thumbs li').eq(settings.thumbFocus - 1).addClass('focus');       
-        elements.carouselWrapper.css('width', defaults.thumbWidth * options.thumbsVisible );                    
-        if ( elements.carouselWrapper.width() >= elements.windowWidth ) {         
-          elements.carouselWrapper.css('margin-left', -((elements.carouselWrapper.width() - elements.windowWidth) / 2) + 35); //hardcoded 35 is half the paddng on one side?
+        defaults.carouselWrapper.css('width', settings.thumbWidth * options.thumbsVisible );                    
+        if ( defaults.carouselWrapper.width() >= $(window).width() ) {         
+          defaults.carouselWrapper.css('margin-left', -((defaults.carouselWrapper.width() - $(window).width()) / 2) + 35); //hardcoded 35 is half the paddng on one side?
         }                             
         functions.updateExcerpt( $('#infinite-thumbs li').eq(settings.thumbFocus - 1).data('index') );        
         $('#infinite-thumbs li').each(function(){   
@@ -58,7 +55,7 @@ jQuery.fn.outerHTML = function(s) {
         }             
       },      
       updateExcerpt: function(index) {        
-        elements.excerptContent.each(function(){
+        options.excerptContent.each(function(){
           $(this).hide();         
           
           if ( $(this).data('index') == index ) {
@@ -78,8 +75,8 @@ jQuery.fn.outerHTML = function(s) {
           settings.inTransition = 1;
           var thumbInFocus = $('#infinite-thumbs li.focus').index() + 1;                      
           if ( thumbInFocus > settings.thumbFocus ) {                                                                                                                                                     
-            elements.thumbWrapper.transition({
-              marginLeft: "+="+defaults.thumbWidth}, options.transitionTime, options.transitionType, function() {
+            options.thumbWrapper.transition({
+              marginLeft: "+="+settings.thumbWidth}, options.transitionTime, options.transitionType, function() {
               $('#infinite-thumbs li').removeClass('focus'); 
               $('#infinite-thumbs li').each(function(){                               
                 if ( $(this).index() + 1 == settings.thumbFocus ) {                                                     
@@ -90,11 +87,11 @@ jQuery.fn.outerHTML = function(s) {
               functions.playSlider();           
             });           
           } else if ( thumbInFocus == settings.thumbFocus ) {                                             
-            elements.thumbWrapper
-              .css('margin-left', -defaults.thumbWidth)
-              .prepend( slideArray[defaults.thumbCount - 1] )             
+            options.thumbWrapper
+              .css('margin-left', -settings.thumbWidth)
+              .prepend( slideArray[settings.thumbCount - 1] )             
               .transition({
-                marginLeft: "+="+defaults.thumbWidth}, options.transitionTime, options.transitionType, function() {
+                marginLeft: "+="+settings.thumbWidth}, options.transitionTime, options.transitionType, function() {
                 this.find('li:last-child').remove();                                            
                   $('#infinite-thumbs li').removeClass('focus');                                        
                 $('#infinite-thumbs li').each(function(){
@@ -108,12 +105,118 @@ jQuery.fn.outerHTML = function(s) {
           }             
         }, options.slideTime);
         settings.inTransition = null;
-      }             
+      },
+      clickRight: function(targetIndex){
+          
+          // @TODO clean up this
+    	  settings.inTransition = 1;
+          variant = targetIndex - settings.thumbFocus;
+          distance = variant * settings.thumbWidth;          
+          var deleteOverflow = 0;
+          
+          // ADD LI'S right of middle
+          for (var i = 0; i < variant; i++) {                                 
+            options.thumbWrapper.append(slideArray[deleteOverflow]);
+            deleteOverflow += 1;            
+          }                                                 
+                                                          
+        options.thumbWrapper.transition({marginLeft: '-='+distance}, options.transitionTime, options.transitionType, function(){           
+          if (options.thumbWrapper.length < settings.thumbCount ) {                                          
+            var i = 0;                             
+            $('#infinite-thumbs li').each(function(){                                               
+              if (i < variant && $(this).index() + 1 != 0) {
+                $(this).remove();                                   
+                i += 1;                 
+              }
+            });             
+            options.thumbWrapper.css('margin-left', '+='+settings.thumbWidth * variant)
+          }            
+          functions.resetArray(); 
+          settings.inTransition = null;
+        }); 
+      },
+      clickLeft: function(targetIndex) {
+          
+        // @TODO clean up this
+    	settings.inTransition = 1;
+        variant = settings.thumbFocus - targetIndex;
+        distance = variant * settings.thumbWidth;        
+        var deleteOverflow = settings.thumbCount - 1;
+        
+                                                
+        options.thumbWrapper.css('marginLeft', '-='+distance);
+                
+        // ADD LI'S left of middle
+        for (var i = 0; i < variant; i++) {                     
+          options.thumbWrapper.prepend(slideArray[deleteOverflow]);
+          deleteOverflow -= 1; 
+        }                                                                                     
+                      
+        options.thumbWrapper.transition({marginLeft: '+='+distance}, options.transitionTime, options.transitionType, function(){       
+          $('#infinite-thumbs li').each(function(){
+            if ($(this).index() + 1 > settings.thumbCount ) {
+              $(this).remove();
+            }
+          });         
+          functions.resetArray();
+          settings.inTransition = null;
+        }); 
+      }
     }
         
     return this.each(function(){
             
-      functions.init();     
+      functions.init();   
+            
+      $('#infinite-nav-right').click(function(){
+    	  
+    	  // duplicated
+          if (settings.inTransition == 1) {
+              return false;
+            }                            
+          
+    	  functions.clickRight( (options.thumbsVisible / 2) + 1 );
+    	  
+          // duplicated               
+          if ( options.playSlider ) {
+            clearTimeout(settings.slideAction);
+            functions.playSlider(); 
+          }
+      });
+      $('#infinite-nav-left').click(function(){
+    	  
+    	  // duplicated
+          if (settings.inTransition == 1) {
+              return false;
+            }
+          
+    	  functions.clickLeft( (options.thumbsVisible / 2) - 1 );
+    	  
+          // duplicated               
+          if ( options.playSlider ) {
+            clearTimeout(settings.slideAction);
+            functions.playSlider(); 
+          } 
+      });
+      
+      
+      if (options.hoverFocus) {
+          $('#infinite-thumbs li img').live({
+      	        mouseenter: function() {
+      	            $(this).addClass('hover');
+      	            $('#infinite-thumbs li img').fadeTo(300,0.2);
+      	        },
+      	        mouseleave: function() {
+      	            $(this).removeClass('hover').css('opacity','1');
+      	            $('#infinite-thumbs li img').stop();
+      	        }
+      		});
+      	    options.thumbWrapper.live({
+      	    	mouseleave: function() {      	    
+      	    		$('#infinite-thumbs li img').stop().fadeTo(150,1);
+      	    	}
+      	  });
+      }
       
       $('#infinite-thumbs li img').live('click', function(e){
         
@@ -125,78 +228,29 @@ jQuery.fn.outerHTML = function(s) {
         var targetIndex = $(this).parents('li').index() + 1;
         $('#infinite-thumbs li').removeClass('focus');
         $(this).parents('li').addClass('focus');
-
+        
         // reset auto-slide timer               
         if ( options.playSlider ) {
           clearTimeout(settings.slideAction);
           functions.playSlider(); 
-        }                             
-          
-        // user clicked focused image
-        if (targetIndex == settings.thumbFocus) {
-          //return false;
-          
-          // user clicked left of middle  
-        } else if ( targetIndex < settings.thumbFocus ) {
-            
-          e.preventDefault();
-          
-            // @TODO clean up this
-          variant = settings.thumbFocus - targetIndex;
-          distance = variant * defaults.thumbWidth;
-          settings.inTransition = 1;
-          var deleteOverflow = defaults.thumbCount - 1;
-          
-                                                  
-          elements.thumbWrapper.css('marginLeft', '-='+distance);
-                  
-          // ADD LI'S left of middle
-          for (var i = 0; i < variant; i++) {                     
-            elements.thumbWrapper.prepend(slideArray[deleteOverflow]);
-            deleteOverflow -= 1; 
-          }                                                                                     
-                        
-          elements.thumbWrapper.transition({marginLeft: '+='+distance}, options.transitionTime, options.transitionType, function(){       
-            $('#infinite-thumbs li').each(function(){
-              if ($(this).index() + 1 > defaults.thumbCount ) {
-                $(this).remove();
-              }
-            });         
-            functions.resetArray();
-            settings.inTransition = null;
-          });         
-          
-          // user clicked right of middle  
-          } else if ( targetIndex > settings.thumbFocus ) {
-            
-            e.preventDefault();
-            
-            // @TODO clean up this
-            variant = targetIndex - settings.thumbFocus;
-            distance = variant * defaults.thumbWidth;
-            settings.inTransition = 1;
-            var deleteOverflow = 0;
-            
-            // ADD LI'S right of middle
-            for (var i = 0; i < variant; i++) {                                 
-              elements.thumbWrapper.append(slideArray[deleteOverflow]);
-              deleteOverflow += 1;            
-            }                                                 
-                                                            
-          elements.thumbWrapper.transition({marginLeft: '-='+distance}, options.transitionTime, options.transitionType, function(){           
-            if (elements.thumbWrapper.length < defaults.thumbCount ) {                                          
-              var i = 0;                             
-              $('#infinite-thumbs li').each(function(){                                               
-                if (i < variant && $(this).index() + 1 != 0) {
-                  $(this).remove();                                   
-                  i += 1;                 
-                }
-              });             
-              elements.thumbWrapper.css('margin-left', '+='+defaults.thumbWidth * variant)
-            }            
-            functions.resetArray(); 
-            settings.inTransition = null;
-          });                   
+        } 
+        
+        if (options.centerClicked) {
+            // user clicked focused image
+            if (targetIndex == settings.thumbFocus) {
+              //return false;
+              
+              // user clicked left of middle  
+            } else if ( targetIndex < settings.thumbFocus ) { 
+            	e.preventDefault();
+            	console.log(targetIndex);
+            	functions.clickLeft(targetIndex);
+            	
+              // user clicked right of middle  
+              } else if ( targetIndex > settings.thumbFocus ) {
+            	e.preventDefault();            	
+                functions.clickRight(targetIndex);
+          	}                            
           }                   
           functions.updateExcerpt( $(this).parents('li').data('index') );       
       });                                                                                                         
